c82666a740ea768bc67d899f50714e3f81f52688
A Coder <ImACoderImACoderImACoder@gmail.com>
2021-11-25 07:04:35 -0500

    css tweak

--- src/components/BleConnection/Ble.jsx
+++ src/components/BleConnection/Ble.jsx
@@ -1,7 +1,7 @@
 import "./Ble.css";
 export default function Ble(props) {
   return (
-    <div className="container">
+    <div>
       <button className="connect-button" onClick={props.onClick}>
         Tap anywhere to connect
       </button>
