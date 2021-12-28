5836157e0af9163faf1bc190b988ab201f4e08f0
A Coder <ImACoderImACoderImACoder@gmail.com>
2021-12-28 10:18:39 -0500

    fixed button jiggle in a hacky way.  The buttons I used aren't designed in a delcarative manner and that is going to cause them to be sluggish.  I'm thinking I might make the fan state local to the component like it used to be.

--- src/components/Volcano/features/FanOn/FanOn.jsx
+++ src/components/Volcano/features/FanOn/FanOn.jsx
@@ -2,16 +2,16 @@ import BootstrapSwitchButton from "bootstrap-switch-button-react";
 
 export default function FanOn(props) {
   return (
-    <div className="heat-air-button">
+    <div onClick={props.onChange} className="heat-air-button">
       <BootstrapSwitchButton
-        onlabel="Fan On"
+        onlabel={"Fan On"}
         offlabel="Fan Off"
         checked={props.isFanOn}
         onstyle="success"
         offstyle="danger"
         // eslint-disable-next-line
         style="heat-air-button"
-        onChange={props.onChange}
+        disabled={true}
       />
     </div>
   );
--- src/components/Volcano/features/FanOn/FanOnContainer.jsx
+++ src/components/Volcano/features/FanOn/FanOnContainer.jsx
@@ -6,8 +6,6 @@ import {
 } from "../../../../constants/uuids";
 import { fanMask } from "../../../../constants/masks";
 import FanOn from "./FanOn";
-import debounce from "lodash/debounce";
-import { bleDebounceTime } from "../../../../constants/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsFanOn } from "../../../../features/deviceInteraction/deviceInteractionSlice";
 import { AddToQueue } from "../../../../services/bleQueueing";
@@ -19,9 +17,26 @@ import {
 import { getCharacteristic } from "../../../../services/BleCharacteristicCache";
 import { useCallback } from "react";
 
-export default function FanOnContainer(props) {
+export default function FanOnContainer() {
   const isFanOn = useSelector((state) => state.deviceInteraction.isFanOn);
+  const isDisabled = useRef(false);
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const blePayload = async () => {
+      const characteristicPrj1V = getCharacteristic(register1Uuid);
+      const value = await characteristicPrj1V.readValue();
+      const currentVal = convertBLEtoUint16(value);
+      const newFanValue = convertToggleCharacteristicToBool(
+        currentVal,
+        fanMask
+      );
+      dispatch(setIsFanOn(newFanValue));
+    };
+    AddToQueue(blePayload);
+  }, [dispatch]);
+
   useEffect(() => {
     const handlePrj1ChangedVolcano = (event) => {
       let currentVal = convertBLEtoUint16(event.target.value);
@@ -29,29 +44,15 @@ export default function FanOnContainer(props) {
         currentVal,
         fanMask
       );
-      if (newFanValue !== isFanOn) {
-        dispatch(setIsFanOn(newFanValue));
-      }
+      dispatch(setIsFanOn(newFanValue));
+      isDisabled.current = false;
     };
-
     const characteristicPrj1V = getCharacteristic(register1Uuid);
-    const blePayload = async () => {
-      characteristicPrj1V.addEventListener(
-        "characteristicvaluechanged",
-        handlePrj1ChangedVolcano
-      );
-      await characteristicPrj1V.startNotifications();
-      const value = await characteristicPrj1V.readValue();
-      const currentVal = convertBLEtoUint16(value);
-      const newFanValue = convertToggleCharacteristicToBool(
-        currentVal,
-        fanMask
-      );
-      if (newFanValue !== isFanOn) {
-        dispatch(setIsFanOn(newFanValue));
-      }
-    };
-    AddToQueue(blePayload);
+    characteristicPrj1V.addEventListener(
+      "characteristicvaluechanged",
+      handlePrj1ChangedVolcano
+    );
+    characteristicPrj1V.startNotifications();
 
     return () => {
       characteristicPrj1V?.removeEventListener(
@@ -59,10 +60,16 @@ export default function FanOnContainer(props) {
         handlePrj1ChangedVolcano
       );
     };
-  }, [dispatch, isFanOn]);
+  }, [dispatch]);
   const spaceBarKeycode = 32;
 
   const onClick = useCallback(() => {
+    if (isDisabled.current) {
+      console.log("Spam click prevented on fan");
+      return;
+    }
+    dispatch(setIsFanOn(!isFanOn));
+    isDisabled.current = true;
     const blePayload = async () => {
       const targetCharacteristicUuid = isFanOn ? fanOffUuid : fanOnUuid;
       const characteristic = getCharacteristic(targetCharacteristicUuid);
@@ -72,18 +79,11 @@ export default function FanOnContainer(props) {
       return `The fan is now ${newFanValue}`;
     };
     AddToQueue(blePayload);
-  }, [isFanOn]);
+  }, [isFanOn, isDisabled, dispatch]);
 
-  const onChange = useCallback(
-    (checked) => {
-      if (checked === isFanOn) {
-        console.log("Fan skipped spam click");
-      } else {
-        onClickDebounceRef.current(onClick);
-      }
-    },
-    [isFanOn, onClick]
-  );
+  const onChange = useCallback(() => {
+    onClick();
+  }, [onClick]);
 
   useEffect(() => {
     const handler = (e) => {
@@ -98,15 +98,11 @@ export default function FanOnContainer(props) {
     };
   }, [onChange, isFanOn]);
 
-  const onClickDebounceRef = useRef(
-    debounce(
-      (onClick) => {
-        onClick();
-      },
-      bleDebounceTime,
-      { isFanOn }
-    )
+  return (
+    <FanOn
+      onChange={onChange}
+      isFanOn={isFanOn}
+      isLoading={isDisabled.current}
+    />
   );
-
-  return <FanOn onChange={onChange} isFanOn={isFanOn} />;
 }
