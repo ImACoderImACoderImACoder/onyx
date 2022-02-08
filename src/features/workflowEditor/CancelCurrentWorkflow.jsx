12e3978d9eec1aad204c157470e67fec36e5b3b0
A Coder <ImACoderImACoderImACoder@gmail.com>
2022-02-08 01:49:52 -0500

    now stopping fan when current workflow is canceled

--- src/features/workflowEditor/CancelCurrentWorkflow.jsx
+++ src/features/workflowEditor/CancelCurrentWorkflow.jsx
@@ -2,6 +2,10 @@ import Button from "./shared/WorkflowFooterButtons";
 import { ClearWorkflowQueue } from "../../services/bleQueueing";
 import { setCurrentWorkflow, setCurrentWorkflowStepId } from "./workflowSlice";
 import { useDispatch } from "react-redux";
+import { AddToQueue } from "../../services/bleQueueing";
+import { fanOffUuid } from "../../constants/uuids";
+import { getCharacteristic } from "../../services/BleCharacteristicCache";
+import { convertToUInt8BLE } from "../../services/utils";
 
 export default function CancelCurrentWorkflow() {
   const dispatch = useDispatch();
@@ -10,6 +14,12 @@ export default function CancelCurrentWorkflow() {
     ClearWorkflowQueue();
     dispatch(setCurrentWorkflowStepId());
     dispatch(setCurrentWorkflow());
+    const blePayload = async () => {
+      const fanOffCharacteristic = getCharacteristic(fanOffUuid);
+      const buffer = convertToUInt8BLE(0);
+      await fanOffCharacteristic.writeValue(buffer);
+    };
+    AddToQueue(blePayload);
   };
 
   return <Button onClick={onClick}>Cancel Active Workflow</Button>;
