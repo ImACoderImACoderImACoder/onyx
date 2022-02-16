79e549974cab77b9da9cac0cdfac229d21a2963d
A Coder <ImACoderImACoderImACoder@gmail.com>
2022-02-15 20:56:11 -0500

    removed some of the default temperatures.  There were so many because it used to be the only option.  I think I added the most popular values, however I will keep an eye out for user feedback

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,3 @@
-import { MAX_CELSIUS_TEMP } from "./temperature";
-
 export const temperatureIncrementedDecrementedDebounceTime = 1000;
 export const localStorageKey = "projectOnyxVapeApp";
 export const patreonLink = "https://www.patreon.com/ImACoderImACoderImACoder";
@@ -8,15 +6,4 @@ export const redditLink = "https://www.reddit.com/user/ImACoderImACoder";
 export const twitterLink = "https://twitter.com/ImmACoder";
 export const instagramLink =
   "https://www.instagram.com/imacoderimacoderimacoder";
-export const defaultTemperatureArray = [
-  180,
-  185,
-  190,
-  195,
-  200,
-  206,
-  212,
-  220,
-  226,
-  MAX_CELSIUS_TEMP,
-];
+export const defaultTemperatureArray = [180, 185, 190, 195, 200, 205];
