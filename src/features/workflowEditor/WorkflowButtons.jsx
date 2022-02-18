cbe647359c4fea285141242634f3bae7c849ea01
A Coder <ImACoderImACoderImACoder@gmail.com>
2022-02-18 05:43:55 -0500

    possible stability improvements to heat on command

--- src/features/workflowEditor/WorkflowButtons.jsx
+++ src/features/workflowEditor/WorkflowButtons.jsx
@@ -184,7 +184,7 @@ export default function WorkFlow() {
                       }
                     };
                     AddToQueue(blePayload);
-                  }, 100)
+                  }, 300)
                 );
               }, 1000)
             );
