e9af9f3160b61ff0b0341c3f00db03026a95239d
Violet <imacoderimacoderimacoder@gmail.com>
2022-07-01 04:02:07 -0400

    updated netlify contact form

--- src/features/deviceInformation/Contact.jsx
+++ src/features/deviceInformation/Contact.jsx
@@ -1,19 +1,28 @@
+import PrideText from "../../themes/PrideText";
+
 export default function Contact(){
+
+    const onClick = (e) => {
+        if (process.env.NODE_ENV === "development"){
+            e.preventDefault()
+            alert('This click doesn\'t do anything in development mode since it only works with Netlify');
+         }
+    }
+
     return (
-        <form name="contact" method="post">
+        <div> 
+            <h1><PrideText text="Contact Me"/></h1>    
+            <p>It's always nice to hear from users.  Maybe you have a feature request or even just want to say hi! If you would like a response be sure to include some kind of contact information in the message.</p>
+            <form name="contact" method="post">
         <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label>Your Name: <input type="text" name="name"/></label>
+          <label>Message: <textarea style={{width: "90vw", height: "25vh", minHeight: "100px"}}name="message"></textarea></label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email"/></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
+          <button onClick={onClick}type="submit">Send</button>
         </p>
       </form>
+        </div>
+        
     )
 }
