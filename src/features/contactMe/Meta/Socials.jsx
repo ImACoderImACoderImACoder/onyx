f831827e13856171f86e14547cf40a2cbefff2a9
Violet <imacoderimacoderimacoder@gmail.com>
2022-07-03 04:00:53 -0400

    Added social icons

--- src/features/contactMe/Meta/Socials.jsx
+++ src/features/contactMe/Meta/Socials.jsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import * as links from "../../../constants/constants";
 import PrideText from "../../../themes/PrideText";
+import GithubIcon from "../../shared/OutletRenderer/icons/brands/GithubIcon";
+import InstagramIcon from "../../shared/OutletRenderer/icons/brands/InstagramIcon";
+import PatreonIcon from "../../shared/OutletRenderer/icons/brands/PatreonIcon";
+import RedditIcon from "../../shared/OutletRenderer/icons/brands/RedditIcon";
+import TwitterIcon from "../../shared/OutletRenderer/icons/brands/TwitterIcon";
 
 const Div = styled.div`
   display: flex;
@@ -9,26 +14,50 @@ const Div = styled.div`
 
 const StyledA = styled.a`
   max-width: min-content;
+  text-decoration: none;
+  color: ${(props) => props.theme.primaryFontColor};
+`;
+
+const SocialDiv = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export default function Socials() {
   return (
     <Div>
-      <h1><PrideText text="Socials"/></h1>
+      <h1>
+        <PrideText text="Socials" />
+      </h1>
       <StyledA href={links.githubLink} target="_blank" rel="noreferrer">
-        Github
-      </StyledA>
-      <StyledA href={links.patreonLink} target="_blank" rel="noreferrer">
-        Patreon
+        <SocialDiv>
+          <GithubIcon />
+          Github
+        </SocialDiv>
       </StyledA>
       <StyledA href={links.redditLink} target="_blank" rel="noreferrer">
-        Reddit
+        <SocialDiv>
+          <RedditIcon />
+          Reddit
+        </SocialDiv>
       </StyledA>
       <StyledA href={links.twitterLink} target="_blank" rel="noreferrer">
-        Twitter
+        <SocialDiv>
+          <TwitterIcon />
+          Twitter
+        </SocialDiv>
       </StyledA>
       <StyledA href={links.instagramLink} target="_blank" rel="noreferrer">
-        Instagram
+        <SocialDiv>
+          <InstagramIcon />
+          Instagram
+        </SocialDiv>
+      </StyledA>
+      <StyledA href={links.patreonLink} target="_blank" rel="noreferrer">
+        <SocialDiv>
+          <PatreonIcon />
+          Patreon
+        </SocialDiv>
       </StyledA>
     </Div>
   );
