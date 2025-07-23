8c2062de4f7c03d38a374233a866fd0a9386cfbf
Imma <imacoderimacoderimacoder@gmail.com>
2025-07-22 21:40:04 -0400

    updated default workflows

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -144,7 +144,7 @@ export const premadeWorkflows = [
   },
   {
     id: 2,
-    name: "Vapesuvius Temp Step Reverse ⏪",
+    name: "Vapesuvius Temp Step ⏪",
     payload: [
       {
         type: "heatOnWithConditions",
@@ -206,198 +206,6 @@ export const premadeWorkflows = [
   },
   {
     id: 3,
-    name: "Really Off",
-    payload: [
-      { type: "heatOff", id: 1 },
-      { type: "setLEDbrightness", id: 2, payload: 0 },
-    ],
-  },
-  {
-    id: 4,
-    name: "Really On",
-    payload: [
-      { type: "heatOn", id: 1, payload: null },
-      { type: "setLEDbrightness", id: 2, payload: 70 },
-    ],
-  },
-  {
-    id: 5,
-    name: "Developer's Special",
-    payload: [
-      { type: "setLEDbrightness", id: 1, payload: 70 },
-      {
-        type: "heatOnWithConditions",
-        id: 1,
-        payload: {
-          default: {
-            temp: 180,
-            wait: 0,
-          },
-          conditions: [
-            {
-              id: 1,
-              ifTemp: 180,
-              nextTemp: 185,
-              wait: 0,
-            },
-            {
-              id: 2,
-              ifTemp: 185,
-              nextTemp: 190,
-              wait: 0,
-            },
-            {
-              id: 3,
-              ifTemp: 190,
-              nextTemp: 195,
-              wait: 0,
-            },
-            {
-              id: 4,
-              ifTemp: 195,
-              nextTemp: 200,
-              wait: 0,
-            },
-          ],
-        },
-      },
-      { type: "fanOn", id: 4, payload: 3.75 },
-      { type: "fanOn", id: 5, payload: 0.5 },
-      { type: "fanOn", id: 6, payload: 0.5 },
-      { type: "fanOn", id: 7, payload: 0.5 },
-      { type: "fanOnGlobal", id: 8, payload: 34 },
-      { type: "heatOff", id: 9 },
-      { type: "setLEDbrightness", id: 10, payload: 0 },
-    ],
-  },
-  {
-    id: 6,
-    name: "Developer's Special Reverse ⏪",
-    payload: [
-      { type: "setLEDbrightness", id: 1, payload: 70 },
-      {
-        type: "heatOnWithConditions",
-        id: 1,
-        payload: {
-          default: {
-            temp: 180,
-            wait: 0,
-          },
-          conditions: [
-            {
-              id: 1,
-              ifTemp: 185,
-              nextTemp: 180,
-              wait: 0,
-            },
-            {
-              id: 2,
-              ifTemp: 190,
-              nextTemp: 185,
-              wait: 0,
-            },
-            {
-              id: 3,
-              ifTemp: 195,
-              nextTemp: 190,
-              wait: 0,
-            },
-            {
-              id: 4,
-              ifTemp: 200,
-              nextTemp: 195,
-              wait: 0,
-            },
-          ],
-        },
-      },
-      { type: "fanOn", id: 4, payload: 3.75 },
-      { type: "fanOn", id: 5, payload: 0.5 },
-      { type: "fanOn", id: 6, payload: 0.5 },
-      { type: "fanOn", id: 7, payload: 0.5 },
-      { type: "fanOnGlobal", id: 8, payload: 34 },
-      { type: "heatOff", id: 9 },
-      { type: "setLEDbrightness", id: 10, payload: 0 },
-    ],
-  },
-  {
-    id: 7,
-    name: "Vapesuvius Temp Step Dosing capsule", //curtesy of Vapesuvius https://i.redd.it/03vbzulhmjhb1.png
-    payload: [
-      {
-        type: "heatOnWithConditions",
-        id: 1,
-        payload: {
-          default: {
-            temp: 185,
-            wait: 5,
-          },
-          conditions: [
-            {
-              id: 1,
-              ifTemp: 185,
-              nextTemp: 197,
-              wait: 5,
-            },
-            {
-              id: 2,
-              ifTemp: 197,
-              nextTemp: 211,
-              wait: 5,
-            },
-            {
-              id: 3,
-              ifTemp: 211,
-              nextTemp: 230,
-              wait: 5,
-            },
-          ],
-        },
-      },
-      { type: "fanOnGlobal", id: 2, payload: 34 },
-      { type: "heatOff", id: 3 },
-    ],
-  },
-  {
-    id: 8,
-    name: "Vapesuvius Temp Step Dosing Capsule Reverse ⏪", //curtesy of Vapesuvius https://i.redd.it/03vbzulhmjhb1.png
-    payload: [
-      {
-        type: "heatOnWithConditions",
-        id: 1,
-        payload: {
-          default: {
-            temp: 185,
-            wait: 5,
-          },
-          conditions: [
-            {
-              id: 1,
-              ifTemp: 197,
-              nextTemp: 185,
-              wait: 5,
-            },
-            {
-              id: 2,
-              ifTemp: 211,
-              nextTemp: 197,
-              wait: 5,
-            },
-            {
-              id: 3,
-              ifTemp: 230,
-              nextTemp: 211,
-              wait: 5,
-            },
-          ],
-        },
-      },
-      { type: "fanOnGlobal", id: 2, payload: 34 },
-      { type: "heatOff", id: 3 },
-    ],
-  },
-  {
-    id: 9,
     name: "Temp Step Whip Loop Full Session", //curtesy of Vapesuvius https://i.redd.it/03vbzulhmjhb1.png
     payload: [
       {
@@ -457,6 +265,198 @@ export const premadeWorkflows = [
       { type: "exitWorkflowWhenTargetTemperatureIs", id: 2, payload: 230 },
       { type: "heatOff", id: 3 },
       { type: "loopFromBeginning", id: 4 },
+    ],
+  },
+  {
+    id: 4,
+    name: "Temp Step Dosing capsule", //curtesy of Vapesuvius https://i.redd.it/03vbzulhmjhb1.png
+    payload: [
+      {
+        type: "heatOnWithConditions",
+        id: 1,
+        payload: {
+          default: {
+            temp: 185,
+            wait: 5,
+          },
+          conditions: [
+            {
+              id: 1,
+              ifTemp: 185,
+              nextTemp: 197,
+              wait: 5,
+            },
+            {
+              id: 2,
+              ifTemp: 197,
+              nextTemp: 211,
+              wait: 5,
+            },
+            {
+              id: 3,
+              ifTemp: 211,
+              nextTemp: 230,
+              wait: 5,
+            },
+          ],
+        },
+      },
+      { type: "fanOnGlobal", id: 2, payload: 34 },
+      { type: "heatOff", id: 3 },
+    ],
+  },
+  {
+    id: 5,
+    name: "Temp Step Dosing Capsule ⏪", //curtesy of Vapesuvius https://i.redd.it/03vbzulhmjhb1.png
+    payload: [
+      {
+        type: "heatOnWithConditions",
+        id: 1,
+        payload: {
+          default: {
+            temp: 185,
+            wait: 5,
+          },
+          conditions: [
+            {
+              id: 1,
+              ifTemp: 197,
+              nextTemp: 185,
+              wait: 5,
+            },
+            {
+              id: 2,
+              ifTemp: 211,
+              nextTemp: 197,
+              wait: 5,
+            },
+            {
+              id: 3,
+              ifTemp: 230,
+              nextTemp: 211,
+              wait: 5,
+            },
+          ],
+        },
+      },
+      { type: "fanOnGlobal", id: 2, payload: 34 },
+      { type: "heatOff", id: 3 },
+    ],
+  },
+  {
+    id: 6,
+    name: "Developer's Special",
+    payload: [
+      { type: "setLEDbrightness", id: 1, payload: 70 },
+      {
+        type: "heatOnWithConditions",
+        id: 1,
+        payload: {
+          default: {
+            temp: 180,
+            wait: 0,
+          },
+          conditions: [
+            {
+              id: 1,
+              ifTemp: 180,
+              nextTemp: 185,
+              wait: 0,
+            },
+            {
+              id: 2,
+              ifTemp: 185,
+              nextTemp: 190,
+              wait: 0,
+            },
+            {
+              id: 3,
+              ifTemp: 190,
+              nextTemp: 195,
+              wait: 0,
+            },
+            {
+              id: 4,
+              ifTemp: 195,
+              nextTemp: 200,
+              wait: 0,
+            },
+          ],
+        },
+      },
+      { type: "fanOn", id: 4, payload: 3.75 },
+      { type: "fanOn", id: 5, payload: 0.5 },
+      { type: "fanOn", id: 6, payload: 0.5 },
+      { type: "fanOn", id: 7, payload: 0.5 },
+      { type: "fanOnGlobal", id: 8, payload: 34 },
+      { type: "heatOff", id: 9 },
+      { type: "setLEDbrightness", id: 10, payload: 0 },
+    ],
+  },
+  {
+    id: 7,
+    name: "Developer's Special Reverse ⏪",
+    payload: [
+      { type: "setLEDbrightness", id: 1, payload: 70 },
+      {
+        type: "heatOnWithConditions",
+        id: 1,
+        payload: {
+          default: {
+            temp: 180,
+            wait: 0,
+          },
+          conditions: [
+            {
+              id: 1,
+              ifTemp: 185,
+              nextTemp: 180,
+              wait: 0,
+            },
+            {
+              id: 2,
+              ifTemp: 190,
+              nextTemp: 185,
+              wait: 0,
+            },
+            {
+              id: 3,
+              ifTemp: 195,
+              nextTemp: 190,
+              wait: 0,
+            },
+            {
+              id: 4,
+              ifTemp: 200,
+              nextTemp: 195,
+              wait: 0,
+            },
+          ],
+        },
+      },
+      { type: "fanOn", id: 4, payload: 3.75 },
+      { type: "fanOn", id: 5, payload: 0.5 },
+      { type: "fanOn", id: 6, payload: 0.5 },
+      { type: "fanOn", id: 7, payload: 0.5 },
+      { type: "fanOnGlobal", id: 8, payload: 34 },
+      { type: "heatOff", id: 9 },
+      { type: "setLEDbrightness", id: 10, payload: 0 },
+    ],
+  },
+  {
+    id: 8,
+    name: "Really Off",
+    payload: [
+      { type: "heatOff", id: 1 },
+      { type: "setLEDbrightness", id: 2, payload: 0 },
+    ],
+  },
+  {
+    id: 9,
+    name: "Really On",
+    payload: [
+      { type: "heatOn", id: 1, payload: null },
+      { type: "setLEDbrightness", id: 2, payload: 70 },
     ],
   },
 ];
